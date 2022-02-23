0a23c1b0209b5ca36501ea9630dba6da0d9df1f6
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-02-22 22:18:05 -0300

    html & css done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+const track = document.querySelector('.track')
+const carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+
+
+next.addEventListener('click', ()=>{
+    track.style.transform = `translate(-${carouselWidth}px)`
+})
+prev.addEventListener('click', ()=>{
+    track.style.transform = `translate(-${0}px)`
+})
